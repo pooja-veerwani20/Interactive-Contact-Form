9dbead50d4484e1be4c275dde2b60c8aa3d1ce6c
pooja-veerwani20 <veerwanipooja4@gmail.com>
2023-11-06 23:18:19 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,72 @@
+
+const form = document.getElementById('form');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const message = document.getElementById('message');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const nameValue = name.value.trim();
+    const emailValue = email.value.trim();
+    const phoneValue = phone.value.trim();
+    const messageValue = message.value.trim();
+
+    if(nameValue === '') {
+        setError(name, 'Name is required');
+    } else {
+        setSuccess(name);
+    }
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if(phoneValue === '') {
+        setError(phone, 'Phone no is required');
+    } else if (phoneValue.length < 8 ) {
+        setError(phone, 'Phone no must be at least 11 character.')
+    } else {
+        setSuccess(phone);
+    }
+
+    if(messageValue === '') {
+        setError(message, 'Please enter your message');
+   
+    } else {
+        setSuccess(message);
+    }
+
+};
